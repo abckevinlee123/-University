a5454cc45e2ea2c27e8afbef1fbfb14f15fc9419
tcslab123 <tcsiotlab123@gmail.com>
2023-12-15 03:47:43 -0500

    Basic navbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import nav_bar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <nav_bar />
+      <p>test</p>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function nav_bar(props) {
+  return (
+    <Navbar>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  );
+}
